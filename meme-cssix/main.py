df0d6630ed2c0371d8be6ecdf4676f8e3de4f61e
Utsab <utsab.k.saha@gmail.com>
2018-07-31 22:40:24 +0000

    Add a meme handler route

--- meme-cssix/main.py
+++ meme-cssix/main.py
@@ -9,17 +9,27 @@ the_jinja_env = jinja2.Environment(
     
 class EnterInfoHandler(webapp2.RequestHandler):
     def get(self):  # for a get request
+        the_variable_dict = {
+            "greeting": "Welcome!!!", 
+            "adjective": "splendid"
+        }
+        
         welcome_template = the_jinja_env.get_template('templates/welcome.html')
-        self.response.write(welcome_template.render())
+        self.response.write(welcome_template.render(the_variable_dict))
 
 
-class MemeHandler(webapp2.RequestHandler):
+class ShowMemeHandler(webapp2.RequestHandler):
     def get(self):  # for a get request
-        welcome_template = the_jinja_env.get_template('templates/results.html')
-        self.response.write(welcome_template.render())
+        results_template = the_jinja_env.get_template('templates/results.html')
+        the_variable_dict = {
+            "line1": "Hurrah for Utsab!", 
+            "line2": "Hurrah!!!!!!!!", 
+            "img_url": "http://www.atozpictures.com/admin/uploads/2016/11/cute-cockatiel-wallpapers.jpg"
+        }
+        self.response.write(results_template.render(the_variable_dict))
 
 
 app = webapp2.WSGIApplication([
     ('/', EnterInfoHandler),
-    ('/showmeme', MemeHandler),
+    ('/showmeme', ShowMemeHandler),
 ], debug=True)
